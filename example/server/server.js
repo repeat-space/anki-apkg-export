39ebdd664ab23b5237eee95b7dd88c457e263a20
Emanuel Fernandes <efernandes@tektorque.com>
2016-09-05 14:40:19 +0100

    add server and browser examples

--- example/server/server.js
+++ example/server/server.js
@@ -5,8 +5,11 @@ const AnkiExport = require('../../dist/index').default;
 
 const apkg = new AnkiExport('deck-name');
 
+apkg.addMedia('anki.png', fs.readFileSync('../assets/anki.png'));
+
 apkg.addCard('card #1 front', 'card #1 back');
 apkg.addCard('card #2 front', 'card #2 back');
+apkg.addCard('card #3 with image <img src="anki.png" />', 'card #3 back');
 
 const zip = apkg.save();
 
