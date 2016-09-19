44e41ed1a109183b917e784160386efb109cbdf5
ewnd9 <ewndnine@gmail.com>
2016-09-19 17:05:23 +0800

    update server example

--- example/server/server.js
+++ example/server/server.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const AnkiExport = require('../../dist/index').default;
 
-const apkg = new AnkiExport('deck-name');
+const apkg = new AnkiExport('deck-name-node');
 
 apkg.addMedia('anki.png', fs.readFileSync('../assets/anki.png'));
 
@@ -11,7 +11,10 @@ apkg.addCard('card #1 front', 'card #1 back');
 apkg.addCard('card #2 front', 'card #2 back');
 apkg.addCard('card #3 with image <img src="anki.png" />', 'card #3 back');
 
-const zip = apkg.save();
-
-fs.writeFileSync('./output.apkg', zip, 'binary');
-console.log(`Package has been generated: output.pkg`);
+apkg
+  .save()
+  .then(zip => {
+    fs.writeFileSync('./output.apkg', zip, 'binary');
+    console.log(`Package has been generated: output.pkg`);
+  })
+  .catch(err => console.log(err.stack || err));
