02ec24cc16e794f88ecc3248423d6bc1f5e4362e
ewnd9 <ewndnine@gmail.com>
2016-09-19 16:49:27 +0800

    add browser-media-ajax example

---
+++ example/browser-media-ajax/index.js
@@ -0,0 +1,23 @@
+import { saveAs } from 'filesaverjs';
+import AnkiExport from 'anki-apkg-export';
+
+const apkg = new AnkiExport('deck-name-ajax');
+
+const params = {
+  method: 'GET'
+};
+
+fetch('https://raw.githubusercontent.com/ewnd9/anki-apkg-export/39ebdd664ab23b5237eee95b7dd88c457e263a20/example/assets/anki.png', params)
+  .then(function(response) {
+    return response.blob();
+  })
+  .then(function(myBlob) {
+    apkg.addMedia('anki.png', myBlob);
+    apkg.addCard('card #1 with image <img src="anki.png" />', 'card #1 back');
+
+    return apkg.save()
+  })
+  .then(function(zip) {
+    saveAs(zip, 'output.apkg');
+  })
+  .catch(err => console.log(err.stack || err));
