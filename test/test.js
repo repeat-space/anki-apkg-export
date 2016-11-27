1bcade39eed92ed89f0228fdee4c4d0037175cb3
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2016-11-27 19:18:55 +0300

    Test data in sqlite db
    Use strict versions
    
    Explicit usage 'use strict'
    
    Use let instead of const

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+"use strict";
 import test from 'ava';
 
 import 'babel-register';
@@ -5,9 +6,21 @@ import 'babel-polyfill';
 
 import AnkiExport from '../src/index';
 import fs from 'fs';
+import  decompress from 'decompress';
+import _ from 'lodash';
+import sqlite3 from 'sqlite3';
+import { exec } from  'child_process';
 
-const dest = '/tmp/result.apkg';
-const sample = __dirname + '/fixtures/output.apkg';
+let SEPARATOR = "\u001F";
+let tmpDir = '/tmp/';
+let dest = tmpDir + 'result.apkg';
+let destUnpacked = tmpDir + 'unpacked_result';
+let destUnpackedDb = destUnpacked + '/collection.anki2';
+let sample = __dirname + '/fixtures/output.apkg';
+
+let addCards = (apkg, list) => list.forEach(({front, back}) => apkg.addCard(front, back));
+
+test.beforeEach(async t => new Promise((resolve) => exec(`rm -rf ${dest} ${destUnpacked}`, resolve)));
 
 test('equals to sample', async t => {
   const apkg = new AnkiExport('deck-name');
@@ -22,12 +35,38 @@ test('equals to sample', async t => {
   fs.writeFileSync(dest, zip, 'binary');
 
   t.ok(zip instanceof Buffer);
+});
 
-  const sampleZip = fs.readFileSync(sample);
-  const destZip = fs.readFileSync(dest);
+test('check internal structure', async t => {
+  // Create deck as in previous example
+  const apkg = new AnkiExport('deck-name');
+  const cards = [
+    { front: 'card #1 front', back: 'card #1 back' },
+    { front: 'card #2 front', back: 'card #2 back' },
+    { front: 'card #3 with image <img src="anki.png" />', back: 'card #3 back' }
+  ];
+  addCards(apkg, cards);
+  const zip = await apkg.save();
+  fs.writeFileSync(dest, zip, 'binary');
 
-  // t.ok(destZip === sampleZip);
-  //
-  // i forgot that there are random number involved, so every time slightly different result
-  // one of solutions could be comparing sql data extracted from the apkg archive
+  // extract dec to tmp directory
+  await decompress(dest, destUnpacked);
+  // analize db via sqlite
+  const db = new sqlite3.Database(destUnpackedDb);
+  const result = await new Promise((resolve, reject) => db.all(
+    `SELECT
+      notes.sfld as front,
+      notes.flds as back
+      from cards JOIN notes where cards.nid = notes.id ORDER BY cards.id`,
+    (err, data) => err ? reject(err) : resolve(data)
+  ));
+  db.close();
+
+  // compare content from just created db with original list of cards
+  const normilizedResult = _.sortBy(result.map(({front, back}) => ({
+    front,
+    back: back.split(SEPARATOR).pop()
+  })), 'front');
+
+  t.ok(_.isEqual(normilizedResult, cards));
 });
