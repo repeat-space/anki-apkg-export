135aeb4aed07e16dc6951fccd7c10ad8d577ec00
Vasiliy Vanchuk <v.vanchuk@tut.by>
2016-11-28 07:40:34 +0300

    Fix notices

--- test/_helpers.js
+++ test/_helpers.js
@@ -1,23 +1,22 @@
-import 'babel-register';
-import 'babel-polyfill';
 import fs from 'fs';
 import path from 'path';
 import Zip from 'jszip';
+import mkdirp from 'mkdirp';
 
 export const addCards = (apkg, list) => list.forEach(({front, back}) => apkg.addCard(front, back));
 
-export  function unzipDeckToDir(pathToDeck, pathToUnzipTo) {
-    !fs.existsSync(pathToUnzipTo) && fs.mkdirSync(pathToUnzipTo);
-
+export const unzipDeckToDir = (pathToDeck, pathToUnzipTo) => {
+    mkdirp.sync(pathToUnzipTo);
     const zipContent = fs.readFileSync(pathToDeck);
     const zip = new Zip();
+
     return zip
             .loadAsync(zipContent, { createFolders: true })
             .then(() => Promise.all(Object.keys(zip.files).map(i => {
                         const file = zip.files[i];
-                        let filePath = path.join(pathToUnzipTo, file.name);
+                        const filePath = path.join(pathToUnzipTo, file.name);
                         return file
                             .async('nodebuffer')
-                            .then((data) => fs.writeFileSync(filePath, data))
+                            .then(data => fs.writeFileSync(filePath, data))
                     })));
 }
