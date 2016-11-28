534462a505d1c4b95f18366ef422663a1d74a116
Vasiliy Vanchuk <v.vanchuk@tut.by>
2016-11-28 07:11:01 +0300

    Remove decompress dependency

--- test/_helpers.js
+++ test/_helpers.js
@@ -1,8 +1,23 @@
 import 'babel-register';
 import 'babel-polyfill';
-
-import decompress from 'decompress';
+import fs from 'fs';
+import path from 'path';
+import Zip from 'jszip';
 
 export const addCards = (apkg, list) => list.forEach(({front, back}) => apkg.addCard(front, back));
 
-export const unzipDeckToDir = (pathToDeck, pathToUnzipTo) => decompress(pathToDeck, pathToUnzipTo);
+export  function unzipDeckToDir(pathToDeck, pathToUnzipTo) {
+    !fs.existsSync(pathToUnzipTo) && fs.mkdirSync(pathToUnzipTo);
+
+    const zipContent = fs.readFileSync(pathToDeck);
+    const zip = new Zip();
+    return zip
+            .loadAsync(zipContent, { createFolders: true })
+            .then(() => Promise.all(Object.keys(zip.files).map(i => {
+                        const file = zip.files[i];
+                        let filePath = path.join(pathToUnzipTo, file.name);
+                        return file
+                            .async('nodebuffer')
+                            .then((data) => fs.writeFileSync(filePath, data))
+                    })));
+}
