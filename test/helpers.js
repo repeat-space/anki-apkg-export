45c1e6b1e87620be369602dbab1bc31d902659c7
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2016-11-30 09:52:13 +0300

    Remove redundant t.plan usage

--- test/helpers.js
+++ test/helpers.js
@@ -16,14 +16,11 @@ import {
 } from '../src/helpers';
 
 test('checksum', t => {
-  t.plan(2);
   t.is(typeof checksum, 'function', 'should be a function');
   t.is(checksum('some string'), 2336613565, 'san calculate checksume for `some string`');
 });
 
 test('getLastItem', t => {
-  t.plan(3);
-
   t.is(typeof getLastItem, 'function', 'should be a function');
   t.is(getLastItem({ a: 0, b: 1 }), 1, 'get value of last object key');
 
@@ -35,21 +32,18 @@ test('getLastItem', t => {
 });
 
 test('getTemplate', t => {
-  t.plan(2);
   t.is(typeof getTemplate, 'function', 'should be a function');
   let template = fs.readFileSync(path.join(__dirname, '../templates/template.sql'), 'utf-8');
   t.is(getTemplate(), template, 'should return correct template');
 });
 
 test('getCssTemplate', t => {
-  t.plan(2);
   t.is(typeof getCssTemplate, 'function', 'should be a function');
   let template = fs.readFileSync(path.join(__dirname, '../templates/template.css'), 'utf-8');
   t.is(getCssTemplate(), template, 'should return correct template');
 });
 
 test('getSql', t => {
-  t.plan(3);
   t.is(typeof getSql, 'function', 'should be a function');
 
   const sql = getSql();
@@ -58,13 +52,11 @@ test('getSql', t => {
 });
 
 test('rand', t => {
-  t.plan(2);
   t.is(typeof rand, 'function', 'should be a function');
   t.is(typeof rand(), 'number', 'should return a number');
 });
 
 test('getDb', t => {
-  t.plan(5);
   t.is(typeof getDb, 'function', 'should be a function');
 
   const db = getDb();
@@ -75,7 +67,6 @@ test('getDb', t => {
 });
 
 test('getZip', t => {
-  t.plan(4);
   t.is(typeof getZip, 'function', 'should be a function');
 
   const zip = getZip();
