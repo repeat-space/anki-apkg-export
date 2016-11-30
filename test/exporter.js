865bb0b2640364eafc6bc66f6ec6375de07c8a04
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2016-11-30 09:49:41 +0300

    Use sinon spy

--- test/exporter.js
+++ test/exporter.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import sinon from 'sinon';
 
 import 'babel-register';
 import 'babel-polyfill';
@@ -6,13 +7,28 @@ import 'babel-polyfill';
 import Exporter from '../src/exporter';
 import { checksum } from '../src/helpers';
 
+let dbMock;
+let zipMock;
+let exporter;
+
+test.beforeEach(() => {
+  dbMock = {
+    export: () => "data",
+    prepare: () => ({getAsObject: () => null })
+  };
+  zipMock = {
+    file: () => null,
+    generateAsync: () => null
+  };
+  exporter = new Exporter(dbMock, zipMock);
+});
+
 test('Exporter class exists', t => {
   t.truthy(new Exporter instanceof Exporter, 'Exporter is constructor');
   t.truthy(new Exporter !== new Exporter, 'Exporter is not singleton');
 });
 
 test('Exporter.addMedia', t => {
-  const exporter = new Exporter({}, {});
   t.truthy(exporter.media instanceof Array);
   t.deepEqual(exporter.media, []);
   exporter.addMedia('some.file', 'data');
@@ -22,53 +38,43 @@ test('Exporter.addMedia', t => {
 });
 
 test('Exporter.save', t => {
-  const dbMock = { export: () => flags['db.export'] = 'Some data' };
-  const zipMock = {
-    file: (path, content) => flags[`zip.file(${path})`] = content,
-    generateAsync: options => flags[`zip.generateAsync`] = options
-  };
-  const flags = {};
-  const exporter = new Exporter(dbMock, zipMock);
+  const dbExportSpy = sinon.spy(dbMock, 'export');
+  const zipFileSpy = sinon.spy(zipMock, 'file');
+  const zipGenerateAsyncSpy = sinon.spy(zipMock, 'generateAsync');
 
-  t.plan(8);
   t.is(typeof exporter.save, 'function', 'should be a function');
 
   exporter.media = [{filename: '1.jpg'}, {filename: '2.bmp'}];
-
   exporter.save({some: 'options', should: { be: 'here'}});
-  const flagsKeys = Object.keys(flags);
-  t.truthy(flagsKeys.includes('db.export'), 'should call .export on db');
-  t.truthy(flagsKeys.includes('zip.file(collection.anki2)'), 'should save notes/cards db');
-  t.truthy(flagsKeys.includes('zip.file(media)'), 'should save media');
-  t.truthy(flagsKeys.includes('zip.file(0)'), 'should save media with two files');
-  t.truthy(flagsKeys.includes('zip.file(1)'), 'should save media with two files');
-  t.truthy(flagsKeys.includes('zip.generateAsync'), 'should call zip.generateAsync');
-  t.truthy(['blob', 'nodebuffer'].includes(flags['zip.generateAsync'].type), 'zip generates binary file');
+
+  t.truthy(dbExportSpy.called, 'should call .export on db');
+  t.truthy(zipFileSpy.calledWithMatch('collection.anki2'), 'should save notes/cards db');
+  t.truthy(zipFileSpy.calledWithMatch('media'), 'should save media');
+  t.truthy(zipFileSpy.calledWithMatch(0), 'should save media with two files');
+  t.truthy(zipFileSpy.calledWithMatch(1), 'should save media with two files');
+  t.truthy(zipGenerateAsyncSpy.called, 'should call zip.generateAsync');
+  t.truthy(['blob', 'nodebuffer'].includes(zipGenerateAsyncSpy.args[0][0].type), 'zip generates binary file');
 });
 
 test('Exporter.addCard', t => {
-  const exporter = new Exporter({}, {});
-
   t.is(typeof exporter.addCard, 'function', 'should be a function');
   const { topDeckId, topModelId, separator } = exporter;
   const [front, back] = [5, 9, '!separator!', 'Test Front', 'Test back'];
-  const results = {};
-  exporter.update = (query, data) => results[query] = data;
+  const exporterUpdateSpy = sinon.spy(exporter, 'update');
+
   exporter.addCard(front, back);
 
-  const keys = Object.keys(results);
-  const notesUpdate = results[`insert into notes values(:id,:guid,:mid,:mod,:usn,:tags,:flds,:sfld,:csum,:flags,:data)`];
-  const cardsUpdate = results[`insert into cards values(:id,:nid,:did,:ord,:mod,:usn,:type,:queue,:due,:ivl,:factor,:reps,:lapses,:left,:odue,:odid,:flags,:data)`];
+  t.is(exporterUpdateSpy.callCount, 2, 'should made two requests');
 
-  t.is(keys.length, 2, 'should made two requests');
-
-  t.truthy(cardsUpdate,'should insert card');
+  t.is(exporterUpdateSpy.args[0][0], `insert into notes values(:id,:guid,:mid,:mod,:usn,:tags,:flds,:sfld,:csum,:flags,:data)`);
+  const notesUpdate = exporterUpdateSpy.args[0][1];
   t.is(notesUpdate[':sfld'], front);
   t.is(notesUpdate[':flds'], front + separator + back);
   t.is(notesUpdate[':mid'], topModelId);
   t.is(notesUpdate[':csum'], checksum(front + separator + back));
 
-  t.truthy(cardsUpdate,'should insert note');
+  t.is(exporterUpdateSpy.args[1][0],`insert into cards values(:id,:nid,:did,:ord,:mod,:usn,:type,:queue,:due,:ivl,:factor,:reps,:lapses,:left,:odue,:odid,:flags,:data)`);
+  const cardsUpdate = exporterUpdateSpy.args[1][1];
   t.is(cardsUpdate[':did'], topDeckId);
   t.is(cardsUpdate[':nid'], notesUpdate[':id'], 'should link both tables via the same note_id');
 });
