ec229588782095daae8cb7cbd84deb0b61519970
Vadim Yakhin <yakhin.v@gmail.com>
2017-06-24 11:44:02 +0300

    fix short loader syntax for webpack 2 (#22)

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ let sql;
 let template;
 
 if (process.env.APP_ENV === 'browser' || typeof window !== 'undefined') {
-  require('script!sql.js');
+  require('script-loader!sql.js');
   sql = window.SQL;
-  template = require('!raw!./../templates/template.sql');
+  template = require('!raw-loader!./../templates/template.sql');
 } else {
   sql = require('sql.js');
   template = require('fs').readFileSync(__dirname + '/../templates/template.sql', 'utf-8');
