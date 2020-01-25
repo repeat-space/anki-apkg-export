e4bd4bedf39d5c5ffba79560139ff6c3f9bdd1c9
ewnd9 <ewndnine@gmail.com>
2020-01-25 20:11:58 +0300

    upgrade to latest sql.js, pass sql as an argument

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import Exporter from './exporter';
 import createTemplate from './template';
 
-let sql;
-
-if (process.env.APP_ENV === 'browser' || typeof window !== 'undefined') {
-  require('script-loader!sql.js');
-  sql = window.SQL;
-} else {
-  sql = require('sql.js');
-}
-
 export { Exporter };
 
-export default function(deckName, template) {
+export default function(deckName, template, sql) {
   return new Exporter(deckName, {
     template: createTemplate(template),
     sql
